b8c078f5e37d4a89fc43df3412a610559fb9861e
Giancarloct90 <giancarloct90@hotmail.com>
2020-07-08 21:35:57 -0600

    working

--- src/app/UI.js
+++ src/app/UI.js
@@ -7,8 +7,20 @@ export class UI {
         this.wind = document.getElementById('weather-wind');
     }
 
-    render(weather) {
+    render(weather, flag) {
+        if (flag) {
+            this.location.textContent = 'City no found, Please try again';
+            this.location.textContent = '';
+            this.desc.textContent = '';
+            this.string.textContent = '';
+            this.humidity.textContent = '';
+            this.wind.textContent = '';
+        }
         this.location.textContent = weather.name + '/' + weather.sys.country;
-        this.desc.textContent = weather.weather[0]['description;']
+        this.desc.textContent = weather.weather[0].description;
+        this.string.textContent = `${weather.main.temp} ℃`;
+        this.humidity.textContent = 'Humidity ' + weather.main.humidity + ' %';
+        this.wind.textContent = 'Wind: ' + weather.wind.speed + ' m/s';
+        // console.log(weather.main.temp);
     }
 }
--- src/app/index.js
+++ src/app/index.js
@@ -1,18 +1,33 @@
 require('./index.css');
+require('./bootstrap.min.css');
 const {
     Weather
 } = require('./Weather.js');
-const weather = new Weather('London', 'uk');
 const {
     UI
 } = require('./UI');
 const ui = new UI();
 
-async function fetchWeather() {
+async function fetchWeather(city, countryCode) {
+    const weather = new Weather(city, countryCode);
     const data = await weather.getWeather();
     const data2 = await data.json();
-    ui.render(data2);
+    console.log(data2);
+    if (data2.cod) {
+        // alert(data2.message);
+        ui.render(data2, true);
+
+    } else {
+        ui.render(data2, false);
+    }
 }
 
+document.getElementById('w-change').addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log(document.getElementById('city').value, document.getElementById('countryCode').value);
+    const city = document.getElementById('city').value;
+    const countryCode = document.getElementById('countryCode').value;
+    fetchWeather(city, countryCode);
+});
 
-document.addEventListener('DOMContentLoaded', fetchWeather());
+// document.addEventListener('DOMContentLoaded', fetchWeather());
